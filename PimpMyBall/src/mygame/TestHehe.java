6a80d711999694c0941c89959d0df2cf2841e3e1
snaggadhagga@gmail.com <snaggadhagga@gmail.com@3db512e1-aa66-e392-7a95-860183b8700b>
2012-05-04 15:38:02 +0000

    FÖRBANNAT!!

---
+++ PimpMyBall/src/mygame/TestHehe.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame;
+
+/**
+ *
+ * @author Jimmy
+ */
+public class TestHehe {
+
+    public TestHehe() {
+        // Kod!?!?!?!?
+        
+        // Nicke, ser du!??
+    }
+}
