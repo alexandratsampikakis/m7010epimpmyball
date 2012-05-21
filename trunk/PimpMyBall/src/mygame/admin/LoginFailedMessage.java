60a23c86a6b67f049cc98e79b6af85a3691f98bf
snaggadhagga@gmail.com <snaggadhagga@gmail.com@3db512e1-aa66-e392-7a95-860183b8700b>
2012-05-21 13:23:34 +0000

    Central server and some messages, not done...

---
+++ trunk/PimpMyBall/src/mygame/admin/LoginFailedMessage.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame.admin;
+
+import com.jme3.network.AbstractMessage;
+import com.jme3.network.serializing.Serializable;
+
+/**
+ *
+ * @author Jimmy
+ */
+@Serializable
+public class LoginFailedMessage extends AbstractMessage {
+    
+    public LoginError error;
+    
+    public LoginFailedMessage() {
+        error = LoginError.NO_ERROR;
+    }
+    
+    public LoginFailedMessage(LoginError error) {
+        this.error = error;
+    }
+}
