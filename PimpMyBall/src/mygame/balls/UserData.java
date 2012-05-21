f044485f962368fb17630cc3e946a3ed5c70157d
nirivonsiri@gmail.com <nirivonsiri@gmail.com@3db512e1-aa66-e392-7a95-860183b8700b>
2012-05-21 13:16:30 +0000

    Level, TestLevel and UserData commited.

---
+++ PimpMyBall/src/mygame/balls/UserData.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame.balls;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.bullet.collision.shapes.SphereCollisionShape;
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.material.Material;
+import com.jme3.math.Vector3f;
+
+/**
+ *
+ * @author nicnys-8
+ */
+public class UserData {
+    //private Vector3f latestCorrectPosition, latestCorrectVelocity;
+    private long id;
+    private String userName;
+    private int rank;
+    private int materialIndex;
+    private Vector3f position;
+    private long bling;
+
+    public UserData(long id) {
+        this.id = id;
+    }
+    
+    public long getId() {
+        return id;
+    }
+    
+    public String getUserName() {
+        return userName;
+    }
+
+}
